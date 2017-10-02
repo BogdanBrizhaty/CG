2e5b4262e90111c1d9fbde97d1e50296af86ec10
BogdanBrizhaty <demaskinas@yandex.ua>
2017-10-02 21:02:49 +0300

    Coordinate axles labels and ITransformale change

--- Lab1/Controls/ITransformable.cs
+++ Lab1/Controls/ITransformable.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Lab1.Controls
 {
     public interface ITransformable
     {
-        void Transform();
+        void Transform(Size newSize, Size oldSize);
     }
 }
