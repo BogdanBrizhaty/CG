045bb44fb2d151c300d142fccbe736d8e62b41a0
BogdanBrizhaty <demaskinas@yandex.ua>
2017-10-04 01:23:49 +0300

    #1. Feature. ColorPicker and ColorEditing of figures on canvas

---
+++ Lab1/Model/BrushInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Lab1.Model
+{
+    public class BrushInfo
+    {
+        public string Caption { get; private set; }
+        public Brush Color { get; private set; }
+        public BrushInfo(string c, Brush color)
+        {
+            Caption = c;
+            Color = color;
+        }
+    }
+}
---
+++ Lab1/ViewModel/Commands/EditColorCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab1.ViewModel.Commands
+{
+    class EditColorCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var vm = parameter as MainViewModel;
+            if (vm != null && vm.SelectedFigure != null)
+            {
+                vm.SelectedFigure.Rect.Stroke = vm.SelectedRectColor.Color;
+                vm.SelectedFigure.Ellipse.Stroke = vm.SelectedEllipseColor.Color;
+            }
+        }
+    }
+}
