649aceb578d87cf06cbfc1e74d158f27366c44d4
BogdanBrizhaty <demaskinas@yandex.ua>
2017-11-27 17:09:33 +0200

    Color List

---
+++ Lab3.Fractals/Colors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Lab3.Fractals
+{
+    public class BrushInfo
+    {
+        public string Name { get; protected set; }
+        public Brush Brush { get; protected set; }
+        public BrushInfo(string name, Brush brush)
+        {
+            Name = name;
+            Brush = brush;
+        }
+    }
+    public static class Colors
+    {
+        private static List<BrushInfo> _colors = new List<BrushInfo>();
+        static Colors()
+        {
+            var res = (typeof(Brushes)).GetProperties();
+            foreach (var i in res)
+            {
+                _colors.Add(new BrushInfo(i.Name,
+                    (Brush)(typeof(Brushes)).GetProperty(i.Name).GetValue(null)));
+            }
+        }
+        public static List<BrushInfo> ColorList
+        {
+            get { return _colors; }
+        }
+    }
+}
